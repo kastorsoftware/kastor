000f3fe87cff823f25507c25221b7cdf9ed4e077
opawee <reenabled1@proton.me>
2026-08-19 23:04:59 +0300

    Increase default action delay

--- src/components/AccountActionsPage.tsx
+++ src/components/AccountActionsPage.tsx
@@ -118,8 +118,8 @@ const defaultConfig: ActionConfig = {
 
   randomizeOrder: false,
   setAutoPhoto: false,
-  delayBetweenMin: 2,
-  delayBetweenMax: 3,
+  delayBetweenMin: 5,
+  delayBetweenMax: 7,
   accountTtl: 0,
   sessionTtl: 0,
 
